5d6a3c6f1bf5146af78c3c99fd228954de4b0b7b
Fakhar <fakhar.oraxtech@gmail.com>
2023-05-04 11:53:28 +0500

    Setting Up The Splashscreen, Using Custom Fonts And How To Find Them

---
+++ app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.projemanag
+
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.projemanag.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    var binding:ActivitySplashBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(binding?.root)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        val face = Typeface.createFromAsset(applicationContext.assets, "carbon bl.ttf")
+        binding?.splashTvTitle?.typeface = face
+    }
+}
