fb859a2cb1626b7db00a873e9793d88e77acac3a
Fakhar <fakhar.oraxtech@gmail.com>
2023-05-04 13:39:50 +0500

    Setting Up The Intro Screen And Moving To It After 2 Second

--- app/src/main/java/com/example/projemanag/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -1,23 +1,17 @@
 package com.example.projemanag
 
-import android.content.Intent
-import android.graphics.Typeface
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
-import com.example.projemanag.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
-    var binding:ActivitySplashBinding? = null
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(binding?.root)
+        setContentView(R.layout.activity_intro)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
@@ -27,11 +21,5 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-        val face = Typeface.createFromAsset(applicationContext.assets, "carbon bl.ttf")
-        binding?.splashTvTitle?.typeface = face
-        Handler().postDelayed({
-                              startActivity(Intent(this,IntroActivity::class.java))
-            finish()
-        },2500)
     }
 }
