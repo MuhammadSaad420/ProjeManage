7bf567bbd984b47f0712531801c5c96fc1345e95
Fakhar <fakhar.oraxtech@gmail.com>
2023-05-05 11:20:50 +0500

    Using The FireStore Database to Store The User Details Part 1

---
+++ app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
@@ -0,0 +1,9 @@
+package com.example.projemanag.firebase
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FirestoreClass {
+    private val firestore = Firebase.firestore;
+
+}
